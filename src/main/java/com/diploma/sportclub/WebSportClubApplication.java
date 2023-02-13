79d5f743b04fb379c3bb118ba09ea322c5accaeb
Lentezz <yaroslav.golovan@gmail.com>
2023-02-13 12:14:53 +0200

    Initial commit

---
+++ src/main/java/com/diploma/sportclub/WebSportClubApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.sportclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSportClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSportClubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diploma/sportclub/WebSportClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.sportclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSportClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
